aa46d2ac23a4386adcaeaacbc27742ef9fb94a7d
PEG-CEO-75 <suzuki.yuua@icloud.com>
2026-05-03 13:03:24 +0900

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -16,12 +16,53 @@ math_a = Subject("数学α","HR教室","大石先生","一次関数")
 social = Subject("社会","HR教室","矢子先生","中国の歴史")
 japanese_b = Subject("国語古典","HR教室","中村(圭)先生","いろは歌")
 #辞書に打ち込む
+#月曜
 monday_timetable = {
     "1限": math_a,
     "2限": social,
     "3限": japanese_b,
+    "4限":,
+    "5限":,
+    "6限":,
 }
-
+#火曜
+tuesday_timetable = {
+    "1限":,
+    "2限":,
+    "3限":,
+    "4限":,
+    "5限":,
+    "6限":,
+}       
+#水曜
+wednesday_timetable = {
+    "1限":,
+    "2限":,
+    "3限":,
+    "4限":,
+    "5限":,
+    "6限":,
+}       
+#木曜
+thursday_timetable = {
+    "1限":,
+    "2限":,
+    "3限":,
+    "4限":,
+    "5限":,
+    "6限":,
+}             
+#金曜
+friday_timetable = {
+    "1限":,
+    "2限":,
+    "3限":,
+    "4限":,
+    "5限":,
+    "6限":,
+}       
+#土曜
+saturday_timetable = 
 #入力欄を st.selectbox に変える。校時の選択。
 st.title("デジタル時間割")
 #曜日の選択
@@ -33,8 +74,6 @@ x = st.radio("何限の授業を見ますか？",["1","2","3","4","5","6"], hori
 if day == "月":
   if f"{x}限"in monday_timetable:
    monday_timetable[f"{x}限"].show_summary()
-  elif x == "安倍晋三":
-   st.write("紫雲院殿政譽清浄晋寿大居士")
   else:
    st.error("その授業はありません。")#エラーを赤く出す
     
